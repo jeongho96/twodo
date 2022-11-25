b3e87b844ab75a46bfd523ca5bc24fd65b7bc47a
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2022-11-25 14:49:55 +0900

    Add files via upload

---
+++ src/main/java/com/f6/twodo/TwodoApplication.java
@@ -0,0 +1,13 @@
+package com.f6.twodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwodoApplication.class, args);
+	}
+
+}
