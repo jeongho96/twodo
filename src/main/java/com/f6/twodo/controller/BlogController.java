363cc834d3e6d9972554f41d5b22513a169a5cc0
seoyoon0 <seoyoon047@gmail.com>
2022-11-28 15:34:15 +0900

    blog 조회 수정

--- src/main/java/com/f6/twodo/controller/BlogController.java
+++ src/main/java/com/f6/twodo/controller/BlogController.java
@@ -1,13 +1,32 @@
 package com.f6.twodo.controller;
 
+import com.f6.twodo.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
-    @GetMapping("/main/blog/insert")
-    public String blogInsert(Model _model) {
-        return "bloginsert";
+
+    BlogService obj_blog_service;
+
+    public BlogController(BlogService _obj){
+        this.obj_blog_service = _obj;
+
     }
+
+    @GetMapping("/test/blog")
+    public String blogList(Model model){
+
+
+        model.addAttribute("BLOG", this.obj_blog_service.getAllBlog());
+
+        System.out.print(model);
+        return "test";
+
+    }
+    //@GetMapping("/main/blog/insert")
+    //public String blogInsert(Model _model) {
+      //  return "bloginsert";
+    //}
 }
--- src/main/java/com/f6/twodo/service/BlogService.java
+++ src/main/java/com/f6/twodo/service/BlogService.java
@@ -1,7 +1,29 @@
 package com.f6.twodo.service;
 
+import com.f6.twodo.mapper.BlogMapper;
+import com.f6.twodo.vo.Blog;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlogService {
+
+    @Autowired
+    private BlogMapper obj_blog_mapper;
+
+    public List<Blog> getAllBlog(){
+        return obj_blog_mapper.getBlogMemory();
+
+
+
+    }
+
+
+    public Integer insertBlog(Blog newrow){
+        Integer rowcount = obj_blog_mapper.addBlog()
+
+    }
+
 }
